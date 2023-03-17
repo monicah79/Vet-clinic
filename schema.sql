1b377714b060d00c177eee8095e7963bb2e6a532
monicah79 <monniewanjema@gmail.com>
2023-03-17 18:00:01 +0300

    query-multiple-tables

--- schema.sql
+++ schema.sql
@@ -10,3 +10,31 @@ CREATE TABLE animals (
 );
 ALTER TABLE animals
 ADD species VARCHAR(100);
+
+CREATE TABLE owners(
+id int generated always as identity primary key,
+full_name varchar(100),
+age int
+);
+
+CREATE TABLE species(
+id int generated always as identity primary key,
+name varchar (100)
+);
+
+ALTER TABLE animals
+DROP species;
+
+ALTER TABLE animals
+ADD species_id int;
+
+ALTER TABLE animals
+ADD constraint fk_species foreign key (species_id)
+REFERENCES species (id);
+
+ALTER TABLE animals
+ADD owners_id int;
+
+ALTER TABLE animals
+ADD constraint fk_owners foreign key (owners_id)
+REFERENCES owners (id);
